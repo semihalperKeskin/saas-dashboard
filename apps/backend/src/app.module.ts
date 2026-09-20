9808d2ab1237fad3d0a9eae7bc83552f5e0fe1fe
semihalperKeskin <semihalperkeskin@gmail.com>
2026-09-21 02:11:24 +0300

    feat(backend): Add Prisma schema configuration and correct module import paths for better organization

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './users/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ColumnModule } from './column';
